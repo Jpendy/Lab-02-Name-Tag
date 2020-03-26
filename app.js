01f026938e2885e928adc058def1d092113ceeee
Jpendy <Jpendy256@gmail.com>
2020-03-26 16:42:22 -0700

    updated

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ console.log('hello world');
 const name = document.getElementById('name');
 
 name.style.color = 'blue';
-name.textContent = 'THE ROCK';
+name.textContent = 'I AM THE ROCK';
 name.style.fontSize = '30px';
 name.style.fontFamily = 'fantasy';
 
 
-//footer = document.getElementsByTagName('footer');
+// const footer = document.getElementsByTagName('footer');
 
-const input = document.createElement('input');
-
+// const input = document.createElement('input');
+// footer[0].appendChild(input);
 
 const myButton = document.getElementById('myButton');
 
 myButton.addEventListener('click', function(){
-    name.textContent = 'I am the Rockkk';
-    //console.log('change the name!');
+    name.textContent = 'I am the Pebble';
+    name.style.color = 'steelblue';
+    console.log('change the name!');
+
 });
+
