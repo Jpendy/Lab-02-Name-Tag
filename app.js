3798f835e19c34a2577e373879a0ba6c14d2d5ae
Jpendy <Jpendy256@gmail.com>
2020-03-26 15:59:52 -0700

    updated nametag with javascript

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+
+const name = document.getElementById('name');
+
+name.style.color = 'blue';
+name.textContent = 'THE ROCK';
+name.style.fontSize = '30px';
+name.style.fontFamily = 'fantasy';
+
+
+//footer = document.getElementsByTagName('footer');
+
+const input = document.createElement('input');
+
+
+
